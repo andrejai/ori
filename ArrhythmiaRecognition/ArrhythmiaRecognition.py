53a6c597dcc7f92c425e3910c3c7be69614284c0
programator <andrejaisakovic@yahoo.com>
2017-07-05 23:39:39 +0200

    Some prints fixed, better output

--- ArrhythmiaRecognition/ArrhythmiaRecognition.py
+++ ArrhythmiaRecognition/ArrhythmiaRecognition.py
@@ -1,9 +1,3 @@
-"""
-    This function loads data from txt file
-    File contains 452 rows of data with 280 column values(attributes) for each row
-    Function removes columns and then rows with missing values or all 0 values
-"""
-
 import os, sys, time
 import numpy as nmp
 import neurolab as nl
@@ -12,7 +6,16 @@ import os.path
 import pylab
 import math
 import sys
-nmp.seterr(divide='ignore', invalid='ignore')
+nmp.seterr(divide='ignore', invalid='ignore') #ignore dividing with 0 problems
+
+#classes of arrhythmia
+CLASS = ["Normal", "Ischemic changes (Coronary Artery Disease)", "Old Anterior Myocardial Infarction",
+         "Old Inferior Myocardial Infarction", "Sinus tachycardy", "Sinus bradycardy",
+         "Ventricular Premature Contraction (PVC)", "Supraventricular Premature Contraction",
+         "Left bundle branch block", "Right bundle branch block", "1. degree AtrioVentricular block",
+         "2. degree AV block", "3. degree AV block", "Left ventricule hypertrophy", "Atrial Fibrillation or Flutter",
+         "Others"]
+
 """
     Correlation of every two columns that don't have missing values is counted.
     Columns represent values of one attribute of EKG
@@ -39,8 +42,10 @@ def seeCorrelation(lines):
 
     correlations = nmp.corrcoef(columns)
 
-    plot.matshow(correlations, cmap=plot.cm.Greens)
+    plot.matshow(correlations, cmap=plot.cm.Greens) #show graph of correlations
     pylab.show()
+
+    #adds only columns that dont have correlation bigger than 0,6
     for i in range(0, len(correlations)):
         for j in range(i+1, len(correlations[0])):
             if i in choosenCols:
@@ -62,7 +67,7 @@ def seeCorrelation(lines):
     Item with value 1 is category in witch arrhythmia is classified.
 """
 def loadData():
-    print("\nLoading only important attributes...")
+    print("\nLoading only important 18 attributes from research...")
     indexes = [0, 4, 48, 75, 90, 102, 111, 162, 166, 168, 172, 198, 206, 210, 260, 266, 270, 276, 279]
     file = open("arrhythmia.data.txt", 'r')
     lines = file.readlines()
@@ -117,7 +122,7 @@ def loadData():
     Returns list of EKG results.
 """
 def loadAllData():
-    print("\nLoading all attributes...")
+    print("\n\nLoading all attributes from file...")
     file = open("arrhythmia.data.txt", 'r')
     lines = file.readlines()
     results = []
@@ -178,8 +183,12 @@ def loadAllData():
             lines[i][j] = lines[i][j] / maxVal
     return lines, results
 
-
+"""
+    Loads only columns with correlations smaller than 0.6
+    This function uses same data set as two functions before.
+"""
 def loadCorrelationCols():
+    print("\n\nLoading only attributes that have correlation smaller than 0.6 for NN...")
     file = open("arrhythmia.data.txt", 'r')
     lines = file.readlines()
     results = []
@@ -225,9 +234,9 @@ def test(net, XTest, YTest):
                     existence += 1
                 elif YTest[i][0] == 0 and j != 0:
                     existence += 1
-    print("\n\n\tPercentage of correctly classified arrhythmia using NN is: {0}%" \
+    print("\t\tPercentage of correctly classified arrhythmia using NN is: {0}%" \
         .format((true/len(XTest))*100))
-    print("\n\n\tPercentage of correctly discovering presence of arrhythmia using classification NN is: {0}%" \
+    print("\t\tPercentage of correctly discovering presence of arrhythmia using classification NN is: {0}%" \
           .format((existence/len(XTest))*100))
 
 """
@@ -241,7 +250,7 @@ def testOneOutput(net, XTest, YTest):
             true += 1
         elif (netOutput[i][0] < 0.6 and YTest[i][0] == 0):
             true += 1
-    print("\n\n\tPercentage of correctly discovered arrhythmia using NN is: {0}%" \
+    print("\t\tPercentage of correctly discovered arrhythmia using NN is: {0}%" \
           .format((true/len(XTest))*100))
 
 
@@ -266,7 +275,8 @@ def splitData(X, Y):
     Making NN and training it if it doesn't exist in path file.
 """
 def setNN(input, output, path, neurons, epochs):
-    print("\n\tResilient backpropagation network, important attributes only...")
+    print("\tResilient backpropagation network with {0} neurons in hidden layer and {1} epochs"\
+          .format(neurons, epochs))
     XInput, YOutput, XTest, YTest = splitData(input, output)
     if os.path.isfile(path):
         print("\n\tLoading network from "+path)
@@ -275,7 +285,8 @@ def setNN(input, output, path, neurons, epochs):
     net = nl.net.newff([[-1, 1]] * len(XInput[0]), [neurons, 16])
     net.trainf = nl.net.train.train_rprop
     net.init()
-    result = net.train(XInput, YOutput, epochs=epochs, show=1, goal=0.0001)
+    result = net.train(XInput, YOutput, epochs=epochs, show=10, goal=0.0001)
+    print("\tSaving network in "+path)
     net.save(path)
     res = net.sim(XInput)
     plot.plot(XInput, YOutput)
@@ -287,11 +298,11 @@ def setNN(input, output, path, neurons, epochs):
             if (res[i][j] == max(res[i])):
                 if (YOutput[i][j] == 1.0):
                     true += 1
-    print(true)
     return net
 
 def setAllNN(input, output, path, neurons):
-    print("\n\tResilient backpropagation network, all attributes...")
+    print("\tResilient backpropagation network with {0} neurons in hidden layer and {1} epochs"\
+          .format(neurons, 50))
     XInput, YOutput, XTest, YTest = splitData(input, output)
     if os.path.isfile(path):
         print("\n\tLoading network from " + path)
@@ -299,8 +310,9 @@ def setAllNN(input, output, path, neurons):
     net = nl.net.newff([[-1, 1]] * len(XInput[0]), [neurons, 16])
     net.trainf = nl.net.train.train_rprop
     net.init()
-    result = net.train(XInput, YOutput, epochs=50, show=1, goal=0.00001)
-    net.save("training_all_30_16.net")
+    result = net.train(XInput, YOutput, epochs=50, show=10, goal=0.00001)
+    print("\tSaving network in "+path)
+    net.save(path)
     res = net.sim(XInput)
     plot.plot(XInput, YOutput)
 
@@ -311,12 +323,11 @@ def setAllNN(input, output, path, neurons):
             if (res[i][j] == max(res[i])):
                 if (YOutput[i][j] == 1.0):
                     true += 1
-    print(true/len(XInput))
-    print(true)
     return net
 
 def setNNOneOutput(input, output, path, neurons, epochs):
-    print("\n\tResilient backpropagation network, recognizing existence of arrhythmia...")
+    print("\tResilient backpropagation network with {0} neurons in hidden layer and {1} epochs"\
+          .format(neurons, epochs))
     XInput, YOutput, XTest, YTest = splitData(input, output)
     if os.path.isfile(path):
         print("\n\tLoading network from " + path)
@@ -325,6 +336,7 @@ def setNNOneOutput(input, output, path, neurons, epochs):
     net.trainf = nl.net.train.train_rprop
     net.init()
     result = net.train(XInput, YOutput, epochs=epochs, show=1, goal=0.0001)
+    print("\tSaving network in "+path)
     net.save(path)
     res = net.sim(XInput)
     plot.plot(XInput, YOutput)
@@ -336,10 +348,11 @@ def setNNOneOutput(input, output, path, neurons, epochs):
             true += 1
         elif (abs(res[i][0]) < 0.6 and YOutput[i][0] == 0):
             true += 1
-    print(true)
     return net
 
-
+"""
+    Return [[1], [0]] when arrhythmia doesn't exist or [[0], [1]] when it does.
+"""
 def decreaseOutput(output):
     result = []
     for line in output:
@@ -357,6 +370,11 @@ def writeTestsToFile(XTest, YTest):
     f.write(testData)
     f.close()
 
+def writeResult(line):
+    f = open('result.txt', 'a')
+    f.write(line)
+    f.close()
+
 if __name__ == '__main__':
 
     #all columns with 30 neurons in hidden layer
@@ -365,7 +383,7 @@ if __name__ == '__main__':
     XInput, YOutput, XTest, YTest = splitData(inp, output)
     test(net_all, XTest, YTest)
 
-    #research columns
+    #research columns with 32 neurons in hidden layer
     inp, output = loadData()
     net_18_100 = setNN(inp, output, "training_18_32_16_100epochs.net", 32, 100)
     XInput, YOutput, XTest, YTest = splitData(inp, output)
@@ -379,9 +397,10 @@ if __name__ == '__main__':
     XInput, YOutput, XTest, YTest = splitData(inp, output)
     testOneOutput(net, XTest, YTest)
 
-    # Columns based on minimal correlation
+    # Columns based on minimal correlation with 30 neurons in hidden layer
     inp, output = loadCorrelationCols()
-    print(len(inp[0]))
+    print("Using {0} columns"\
+            .format(len(inp[0])))
     net_corr = setNN(inp, output, "training_correlation_cols.net", 30, 200)
     print("\n\tAttributes that are used were picked based on correlations between attributes.")
     XInput, YOutput, XTest, YTest = splitData(inp, output)
@@ -397,10 +416,10 @@ if __name__ == '__main__':
         res = net_18_100.sim(line)
         res = list(res)
         res[0] = list(res[0])
-        print("Arrhythmia class: {0}" \
-              .format(res[0].index(max(res[0]))+1))
+        print("Arrhythmia class: {0} ({1})" \
+              .format(res[0].index(max(res[0]))+1, CLASS[res[0].index(max(res[0]))]))
         if(res[0].index(max(res[0])) == 0):
             print("Arrhythmia is not recognized! You don't have arrhythmia!")
-        user_input = input("Enter data, press enter to finish: ")
+        user_input = input("Enter data, press enter to exit: ")
 
 
--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Levenberg-Marquardt algoritma(metoda najmanjih kvadrata) za klasifikaciju aritmi
 Dataset skup sa sajta https://archive.ics.uci.edu/ml/datasets/Arrhythmia. Potrebno je izbaciti redove sa nedostajućim vrednostima i redove čiji su sadržaj sve nule. Takođe je potrebno malo smanjiti broj kolona, koristiće se samo kolone(atributi) koji su najviše povezani sa pojavljivanjem aritmije.
 
 Kao metrika za poređenje koristiće se procenat tačnog određivanja pojave aritmije i procenat tačne klasifikacije aritmije. Dataset će biti podeljen na tri dela, najveći deo će biti podaci za treniranje neuronske mreže(oko 75%), podaci za validaciju(oko 15%) i test podaci(oko 10%).
+
+Pokretanje:
+Kako bi se ponovo istrenirale neuronske mreže potrebno je obrisati .net fajlove, ako želite da učitate već postojeće mreže samo pokrenuti ArrhythmiaRecognition.py.
+Program ispisuje koje se mreže koriste, sa kojim podacima i sa kojom tačnošću. Na kraju nudi da se unesu atributi(jedan red iz test.txt fajla iz [] zagrada) i ispisuje rezultate neuronske mreže.
+
